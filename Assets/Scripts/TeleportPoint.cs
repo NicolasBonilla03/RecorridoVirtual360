45aee4a4a721bd272d17f54a5ebdb5fb2429935c
NicolasBonilla03 <inbonilla@uniboyaca.edu.co>
2026-09-06 19:10:32 -0500

    Update TeleportPoint.cs

--- Assets/Scripts/TeleportPoint.cs
+++ Assets/Scripts/TeleportPoint.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TeleportPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Configuración del Teletransporte")]
+    public string nombreDestino = "Destino";
+    public Material skyboxDestino;
+
+    [Header("Referencias UI")]
+    public GameObject panelConfirmacion;
+    public TextMeshProUGUI textoDestino;
+
+    private bool panelAbierto = false;
+
     void Start()
     {
-        
+        if (panelConfirmacion != null)
+        {
+            panelConfirmacion.SetActive(false);
+            if (textoDestino != null)
+                textoDestino.text = "¿Ir a " + nombreDestino + "?";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AlHacerClick()
     {
-        
+        panelAbierto = !panelAbierto;
+        if (panelConfirmacion != null)
+            panelConfirmacion.SetActive(panelAbierto);
+    }
+
+    public void Confirmar()
+    {
+        if (skyboxDestino != null)
+        {
+            FadeController.Instance.CambiarSkybox(skyboxDestino);
+            CerrarPanel();
+        }
+        else
+        {
+            Debug.LogWarning("No hay skybox asignado para: "
+                           + nombreDestino);
+        }
+    }
+
+    public void CerrarPanel()
+    {
+        panelAbierto = false;
+        if (panelConfirmacion != null)
+            panelConfirmacion.SetActive(false);
     }
 }
